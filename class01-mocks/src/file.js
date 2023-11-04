b21fdc9ee16891cc989274a64325a9f75a85480e
paulohlips <paulohfcmendes@gmail.com>
2023-11-04 17:19:53 -0300

    feat: test file length error

--- class01-mocks/src/file.js
+++ class01-mocks/src/file.js
@@ -1,5 +1,26 @@
-class File {
+const { readFile } = require("fs/promises")
+const { errors } = require("./constants")
+const DEFAULT_OPTION = {
+  maxLines: 3,
+  fields: ["id", "name", "profession", "age"]
+}
 
+class File {
+  static async csvToJson(filePath) {
+    const content = await readFile(filePath, "utf8")
+    const validation = this.isValid(content)
+    if(!validation.valid) throw new Error(validation.error)
+  }
+
+  static isValid(csvString, options = DEFAULT_OPTION) {
+    const [headers, ...fileWithOutHeaders] = csvString.split(/\r?\n/)
+    if (!fileWithOutHeaders.filePath) {
+      return {
+        error: errors.FILE_LENGTH_ERROR_MESSAGE,
+        valid: false
+      }
+    }
+  }
 }
 
 module.exports = File
